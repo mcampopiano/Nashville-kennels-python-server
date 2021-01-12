d10443b7a5c386d0dbbe82993ce35e8b383728ae
Mario Campopiano <mcampopiano92@gmail.com>
2021-01-12 13:26:00 -0600

    added delete_customer

--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
-from customers import get_all_customers, get_single_customer, create_customer
+from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 
 
@@ -130,6 +130,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             delete_location(id)
         elif resource == "employees":
             delete_employee(id)
+        elif resource == "customers":
+            delete_customer(id)
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
