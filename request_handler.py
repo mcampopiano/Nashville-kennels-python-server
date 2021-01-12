d99682a5813fa054c4dd126f25e3a31267e9f4ae
Mario Campopiano <mcampopiano92@gmail.com>
2021-01-12 15:53:12 -0600

    added PUT method to employees

--- request_handler.py
+++ request_handler.py
@@ -3,7 +3,7 @@ import json
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
 
 
 # Here's a class. It inherits from another class.
@@ -129,6 +129,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             update_location(id, post_body)
         elif resource == "customers":
             update_customer(id, post_body)
+        elif resource == "employees":
+            update_employee(id, post_body)
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
