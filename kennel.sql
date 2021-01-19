5378c03fc94156ffe1732aa2be204bdd15c1895d
Mario Campopiano <mcampopiano92@gmail.com>
2021-01-19 15:37:48 -0600

    added embedded data for locations to animals

--- kennel.sql
+++ kennel.sql
@@ -70,3 +70,16 @@ WHERE a.id = 3;
 DROP TABLE employee;
 
 SELECT * FROM animal;
+
+SELECT
+    a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id,
+    l.name location_name,
+    l.address location_address
+FROM Animal a
+JOIN Location l
+    ON l.id = a.location_id;
